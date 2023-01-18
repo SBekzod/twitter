bfcf271b263a1845fc5f4700dfac226861fd2257
martin <martin@comets.kr>
2023-01-18 16:08:54 +0900

    feat: form view of tweets with HttpResponse

--- train.py
+++ train.py
@@ -1,9 +1,18 @@
+
 def powersum(power, *args):
-    #Return the sum of each argument raised to the specified power.
     total = 0
     for i in args:
         total += pow(i, power)
     return total
 
-#function call
 print(powersum(2, 3, 4))
+print('======================')
+
+def myFun(**kwargs):
+    print('KWARGS:', kwargs)
+    for key, value in kwargs.items():
+        print("%s == %s" % (key, value))
+ 
+ 
+# Driver code
+myFun(first='Geeks', mid='for', last='Geeks')
