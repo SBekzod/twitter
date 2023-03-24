5a9c053b01eb82b527fc951c474b0d865111b555
martin <martin@comets.kr>
2023-03-24 18:00:37 +0900

    feat: serialize model object business logic

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,4 @@
+import random
 from django.db import models
 
 
@@ -5,6 +6,11 @@ class Tweet(models.Model):
     # Maps to SQL data
     # id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
-    image = models.FileField(upload_to='images/', blank=True, null=True)
-    
-   
+    image = models.FileField(upload_to="images/", blank=True, null=True)
+
+    def serialize(self):
+        return {
+            "id": self.content,
+            "content": self.content,
+            "likes": random.randint(0, 200),
+        }
